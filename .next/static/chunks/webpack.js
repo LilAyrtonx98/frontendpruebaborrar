290515dee6945dce79726eb41147e6d7ad7019c8
Ayrton <ayrton.huenchual@usach.cl>
2022-01-16 03:15:31 -0300

    Pruebas

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "53dad0fc76918af3"; }
+/******/ 		__webpack_require__.h = function() { return "7e9dcf191969d35c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
